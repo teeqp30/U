534030096a341224129c8590e99bc17545fd9c18
Wolf Gps <wolfgps@build.com>
2026-07-16 09:24:47 +0000

    Fix: Update headers to match Tweak.x requirements and fix build errors

--- WolfoxSpoofOverlay.h
+++ WolfoxSpoofOverlay.h
@@ -2,28 +2,8 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 
-@interface WolfoxSpoofOverlay : UIView <UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
-
-@property (nonatomic, strong) UITextField *searchField;
-@property (nonatomic, strong) UIButton *activateBtn;
-@property (nonatomic, strong) UIButton *closeBtn;
-@property (nonatomic, strong) UIButton *myLocationBtn;
-@property (nonatomic, strong) UIButton *infoBtn;
-@property (nonatomic, strong) UIButton *cameraBtn;
-@property (nonatomic, strong) UIButton *settingsBtn;
-@property (nonatomic, strong) UISegmentedControl *mapTypeControl;
-@property (nonatomic, strong) UISwitch *jitterSwitch;
-@property (nonatomic, strong) UISwitch *cameraSwitch;
-@property (nonatomic, strong) UITableView *favoritesTable;
-@property (nonatomic, strong) UITextField *identifierField;
-@property (nonatomic, strong) UITextField *hideCountField;
-@property (nonatomic, strong) UIView *mapContainer;
-@property (nonatomic, strong) UIView *identifierSection;
-@property (nonatomic, strong) UIView *cameraSection;
-@property (nonatomic, strong) UIView *hideSection;
-
-- (instancetype)initWithFrame:(CGRect)frame;
+@interface WolfoxSpoofOverlay : UIViewController <UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
++ (instancetype)shared;
 - (void)refreshActivationState;
 - (void)showActivationAlert;
-
 @end
