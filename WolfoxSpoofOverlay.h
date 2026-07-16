07d4851e14473d4d4bd60d77cea792220f992e58
Wolf Gps <wolfgps@build.com>
2026-07-16 09:26:06 +0000

    Update: Merge latest V3 headers and finalize build requirements

--- WolfoxSpoofOverlay.h
+++ WolfoxSpoofOverlay.h
@@ -1,9 +1,59 @@
-#pragma once
 #import <UIKit/UIKit.h>
-#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
 
-@interface WolfoxSpoofOverlay : UIViewController <UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@interface WolfoxSpoofOverlay : UIViewController <MKMapViewDelegate, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource, CBCentralManagerDelegate>
+@property (nonatomic, strong) UIButton *gpsBtn;
+@property (nonatomic, strong) UIVisualEffectView *panel;
+@property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, strong) UIView *controlsContainer;
+@property (nonatomic, strong) MKMapView *map;
+@property (nonatomic, strong) UIButton *expandMapBtn;
+@property (nonatomic, strong) UISegmentedControl *mapTypeControl;
+@property (nonatomic, strong) MKPointAnnotation *pin;
+@property (nonatomic, strong) UISearchBar *searchBar;
+
+// Favorites
+@property (nonatomic, strong) UIVisualEffectView *favView;
+@property (nonatomic, strong) UITableView *table;
+
+// Bluetooth Scanner
+@property (nonatomic, strong) UIVisualEffectView *btView;
+@property (nonatomic, strong) UITableView *btTable;
+@property (nonatomic, strong) UILabel *btStatusLabel;
+@property (nonatomic, strong) CBCentralManager *cbManager;
+@property (nonatomic, strong) NSMutableArray *discoveredDevices;
+
+// Controls
+@property (nonatomic, strong) UIButton *mainActionBtn;
+@property (nonatomic, strong) UISwitch *jitterSwitch;
+@property (nonatomic, strong) UISlider *jitterSlider;
+@property (nonatomic, strong) UILabel *jitterLabel;
+
+// Route Simulation UI
+@property (nonatomic, assign) BOOL isRouteModeEnabled;
+@property (nonatomic, strong) MKPointAnnotation *startPin;
+@property (nonatomic, strong) MKPointAnnotation *endPin;
+@property (nonatomic, strong) MKPolyline *routeLine;
+@property (nonatomic, strong) UILabel *speedLabel;
+@property (nonatomic, strong) NSTimer *routeTimer;
+
+// Restart Modal
+@property (nonatomic, strong) UIVisualEffectView *confirmDialogBackdrop;
+@property (nonatomic, strong) UIView *confirmDialogView;
+@property (nonatomic, strong) UILabel *timerLabel;
+@property (nonatomic, assign) NSInteger countdownTimer;
+@property (nonatomic, strong) NSTimer *restartTimer;
+@property (nonatomic, assign) BOOL isPendingRestart;
+@property (nonatomic, assign) BOOL isMapExpanded;
+
+@property (nonatomic, strong) NSTimer *jitterTimer;
+@property (nonatomic, assign) BOOL toolHidden;
+@property (nonatomic, assign) CGFloat panelWidth;
+@property (nonatomic, assign) CGFloat panelHeight;
+@property (nonatomic, assign) CGFloat collapsedMapHeight;
+
 + (instancetype)shared;
-- (void)refreshActivationState;
-- (void)showActivationAlert;
+- (void)hideToolCompletely;
+- (void)showToolGesture;
 @end
--- WolfoxSpoofStore.h
+++ WolfoxSpoofStore.h
@@ -1,41 +1,25 @@
-#pragma once
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
-#import <UIKit/UIKit.h>
 
 @interface WolfoxSpoofStore : NSObject
-// الموقع
+@property (nonatomic, assign) BOOL toolHidden;
 @property (nonatomic, assign) CLLocationCoordinate2D fakeCoords;
 @property (nonatomic, assign) BOOL isActive;
-@property (nonatomic, assign) BOOL hasStoredLocation;
-// الارتجاج والحركة
 @property (nonatomic, assign) BOOL isJitterActive;
 @property (nonatomic, assign) double jitterDistance;
-@property (nonatomic, assign) BOOL isRouteActive;
-@property (nonatomic, assign) CLLocationCoordinate2D currentMovingCoords;
-@property (nonatomic, assign) double travelSpeed;
-// الانجراف
+@property (nonatomic, strong) NSMutableArray<NSDictionary *> *favorites;
+@property (nonatomic, assign) BOOL hasStoredLocation;
 @property (nonatomic, assign) double driftLatitude;
 @property (nonatomic, assign) double driftLongitude;
-// المفضلة
-@property (nonatomic, strong) NSMutableArray<NSDictionary *> *favorites;
-// الإخفاء الذكي
-@property (nonatomic, assign) BOOL toolHidden;
-@property (nonatomic, assign) BOOL isPermanentlyHidden;
-@property (nonatomic, assign) NSInteger hideClickCount;
-// محاكاة الكاميرا
-@property (nonatomic, assign) BOOL cameraSimEnabled;
-@property (nonatomic, strong) UIImage *simulatedCameraImage;
-// التفعيل
-@property (nonatomic, strong) NSString *activationCode;
-@property (nonatomic, strong) NSString *deviceUUID;
-@property (nonatomic, assign) BOOL isActivated;
-@property (nonatomic, strong) NSString *expiresAt;
-// نوع الخريطة
-@property (nonatomic, assign) NSInteger mapType; // 0=عادي 1=قمر صناعي 2=مختلط
+
+// Route Simulation
+@property (nonatomic, assign) BOOL isRouteActive;
+@property (nonatomic, assign) CLLocationCoordinate2D startCoords;
+@property (nonatomic, assign) CLLocationCoordinate2D endCoords;
+@property (nonatomic, assign) double travelSpeed;
+@property (nonatomic, assign) CLLocationCoordinate2D currentMovingCoords;
 
 + (instancetype)shared;
 - (void)save;
 - (void)load;
-- (NSString *)getDeviceUUID;
 @end
